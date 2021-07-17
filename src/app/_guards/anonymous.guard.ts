f31f7cdf3773b7452960280f74e1847a418eb36e
yavor300 <yavor300@gmail.com>
2021-07-17 15:28:55 +0300

    Added anonymous guard for the routes

---
+++ src/app/_guards/anonymous.guard.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {UserService} from '../_services/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnonymousGuard implements CanActivate {
+  constructor(private router: Router, private userService: UserService) {
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+    if (!this.userService.isLoggedIn()) {
+      return true;
+    }
+    this.router.navigate(['dashboard']);
+    return false;
+  }
+}
