d20268afda28af87532e083c7af1c4c3c3ebd99b
yavor300 <yavor300@gmail.com>
2021-07-17 09:14:38 +0300

    Fixed site title. Added touting module. Added dashboard module.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // { path: 'home', component: HomeComponent, canActivate: [AnonymousGuard] },
+  // { path: 'login', component: LoginComponent, canActivate: [AnonymousGuard] },
+  // { path: 'register', component: RegisterComponent },
+  // { path: 'dashboard', component: DashboardComponent, canActivate: [RoleGuard], data: {role: 'ROLE_DEV'} },
+  // { path: 'courses/:id', component: CourseComponent, canActivate: [RoleGuard], data: {role: 'ROLE_STUDENT'} },
+  // { path: 'admin', component: AdminComponent, canActivate: [RoleGuard], data: {role: 'ROLE_ADMIN'} },
+  // { path: 'profile/student/:name', component: StudentProfileComponent, canActivate: [RoleGuard], data: {role: 'ROLE_DEV'} },
+  // { path: 'profile', component: ProfileComponent, canActivate: [RoleGuard], data: {role: 'ROLE_DEV'} }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
